343272b9eb5cf043381a299db82e7b39991a1bf2
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-09-22 11:47:05 +0900

    commit

--- includes/config.php
+++ includes/config.php
@@ -11,7 +11,7 @@ date_default_timezone_set("Europe/Prague");
 
 try{
     $db=new PDO('mysql:dbname=heroku_14a10962e3573dd;host=us-cdbr-east-02.cleardb.com;charset=utf8','b61016b7fbd6c3','73dd0225');
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }catch(PDOException $e){
     print('DB接続エラー:'.$e->getMessage());
 }
