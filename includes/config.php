cd035d180b790d73b256a8ad15b657dd76d27127
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-09-22 10:05:25 +0900

    commit

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,17 @@
+
+
+
 <?php
+//例外処理
+
 ob_start(); //Turns on output buffering 
 session_start();
 
 date_default_timezone_set("Europe/Prague");
 
-try {
-    $con = new PDO("mysql:dbname=dentakuno2;host=localhost", "root", "");
+try{
+    $db=new PDO('mysql:dbname=heroku_14a10962e3573dd;host=us-cdbr-east-02.cleardb.com;charset=utf8','b61016b7fbd6c3','73dd0225');
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}catch(PDOException $e){
+    print('DB接続エラー:'.$e->getMessage());
 }
-catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
